40a1e9b28fd5df492acd9960fdd41e7b05363b5f
Victor <victorm.martinezjr@outlook.com>
2026-02-13 21:16:15 -0500

    adjusted text size in hero and details sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="min-h-screen w-full text-white bg-black px-4 overflow-x-hidden xl:px-8">
+    <main className="min-h-screen w-full text-white bg-black px-4 overflow-x-hidden text-xl xl:px-8">
       <Navbar />
       <Hero />
       <Details />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,28 @@ import hero from "../assets/hero.png";
 
 const Hero = () => {
   return (
-    <div className="h-[92vh] w-full flex flex-col justify-around items-center text-xl my-4">
+    <div className="h-[92vh] w-full flex flex-col justify-around items-center my-4">
       {/* Hero Text */}
       <div className="w-full flex flex-col items-center text-center gap-5 xl:gap-10">
         <h1 className="text-4xl md:text-7xl lg:text-8xl xl:text-9xl">
-          Luxury car detailing
+          Luxury Car Detailing
         </h1>
-        <p className="text-gray-500 xl:text-2xl xl:w-[40%]">
+        <p className="text-gray-500 text-xl xl:text-2xl xl:w-[40%]">
           Experience the prestige of a professional detailed car, radiating
           elegance and refinement at every turn.
         </p>
-        <button className="flex gap-2 xl:text-2xl">
+        <button className="flex gap-2 text-2xl">
           Let's connect <GoArrowUpRight />
         </button>
       </div>
 
       {/* Hero Image */}
       <div className="w-full min-h-0 flex items-center justify-center">
-        <img src={hero} alt="Luxury car" className="max-h-full max-w-full " />
+        <img
+          src={hero}
+          alt="Luxury car"
+          className="max-h-full max-w-full transform scale-150 md:scale-100"
+        />
       </div>
     </div>
   );
